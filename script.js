d4eec51f7a1a162c6bea46085289ca34d64e6bf9
manukingg <hatbotinok@mail.ru>
2022-04-09 21:13:50 +0400

    done 2nd part of excercise

---
+++ script.js
@@ -0,0 +1,35 @@
+let screenWidth = window.innerHeight;
+let containerSize = screenWidth * 0.45;
+let cellAmount = 16
+let cellSize = containerSize/cellAmount;
+let body = document.querySelector('body');
+let gridContainer = document.createElement('div');
+
+function createContainer(containerSize){
+    gridContainer.classList.add('cellContainer');
+    gridContainer.style.height = `${containerSize}px`;
+    gridContainer.style.width = `${containerSize}px`;
+    body.appendChild(gridContainer);
+
+}
+
+function createCell(cellSize){
+    const gridElement = document.createElement('div');
+    gridElement.style.height = `${cellSize}px`;
+    gridElement.style.width = `${cellSize}px`;
+    gridElement.classList.add('gridCell');
+    return gridElement;
+}
+
+function createGrid(cellAmount){
+    createContainer(containerSize);
+    for (let i = 0; i < cellAmount; i++){
+        for (let j = 0; j < cellAmount; j++){
+            gridContainer.appendChild(createCell(cellSize));
+        }
+    }
+}
+
+createGrid(cellAmount);
+
+window.addEventListener()
